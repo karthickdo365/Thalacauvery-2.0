200604cd6110ed50049b211312346c7293d21d22
Karthick <karthickdo365@gmail.com>
2026-08-24 12:37:49 +0530

    Fix frontend build output for Render

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,21 +3,40 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+
   build: {
-    outDir: '../../backend/server/public',
+    outDir: 'dist',
     emptyOutDir: true,
+
     rollupOptions: {
       output: {
         manualChunks: {
-          react:  ['react', 'react-dom', 'react-router-dom', 'react-redux', '@reduxjs/toolkit'],
-          mui:    ['@mui/material', '@mui/icons-material', '@mui/x-date-pickers'],
-          charts: ['chart.js', 'react-chartjs-2'],
+          react: [
+            'react',
+            'react-dom',
+            'react-router-dom',
+            'react-redux',
+            '@reduxjs/toolkit',
+          ],
+
+          mui: [
+            '@mui/material',
+            '@mui/icons-material',
+            '@mui/x-date-pickers',
+          ],
+
+          charts: [
+            'chart.js',
+            'react-chartjs-2',
+          ],
         },
       },
     },
   },
+
   server: {
     port: 3000,
+
     proxy: {
       '/api': 'http://localhost:5000',
       '/uploads': 'http://localhost:5000',
